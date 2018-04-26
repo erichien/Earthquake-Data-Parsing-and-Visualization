c4976e3e9f515869fce21d3654f8dcd841a3f502
Eric <17807730+virtuositeit@users.noreply.github.com>
2018-04-26 13:58:18 -0400

    recovered markup

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,20 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('load', function(event) {
-      let width = 1200;
-      let height = 600;
+      let width = 1000;
+      let height = 800;
       let inputValue = null;
 
       let svg = d3
         .select('.d3')
         .append('svg')
         .attr('width', width)
-        .attr('height', height);
+        .attr('height', height)
+        .attr('translate', width / 3);
 
       let g = svg.append('g');
 
-      let earthProjection = d3
-        .geoAlbersUsa()
-        .translate([width / 2, height / 2]);
+      let earthProjection = d3.geoAlbersUsa();
 
       let geoPath = d3.geoPath().projection(earthProjection);
 
@@ -25,7 +24,7 @@
         .enter()
         .append('path')
         .attr('fill', '#eee')
-        .attr('stroke', '#888')
+        .attr('stroke', '#aaa')
         .attr('d', geoPath);
 
       let rodents = svg.append('g');
@@ -40,11 +39,13 @@
         .attr('d', geoPath)
         .attr('class', 'incident')
         .on('mouseover', function(d) {
-          d3.select('h2').text(d.properties.place);
+          d3.select('.d3-data').text(d.properties.place);
           d3.select(this).attr('class', 'incident hover');
         })
         .on('mouseout', function(d) {
-          d3.select('h2').text('');
+          d3
+            .select('.d3-data')
+            .text('(hover over data points to view earthquake info)');
           d3.select(this).attr('class', 'incident');
         });
     });
